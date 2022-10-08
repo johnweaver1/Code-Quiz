81b708beb1e027bea8071abe50e7d238f728a786
John W <john.weaver94@utexas.edu>
2022-10-07 22:09:30 -0500

    Added the questions in a seperate .js file. Will link them in later.

---
+++ assets/js/quizQuestions.js
@@ -0,0 +1,22 @@
+let questions = [
+    {
+        question: "Commonly used data types DO Not Include:",
+        answers: ["strings", "booleans", "alerts", "numbers"],
+        correct: "alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _______.",
+        answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        correct: "parentheses"
+    },
+    {
+        question: "A very useful tool used during devlopment and debugging for printing content to the debuger is:",
+        answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        correct: "console.log"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store",
+        answers: ["numbers and string", "other arrays", "booleans", "all of the above"],
+        correct: "all of the above"
+    },
+];
